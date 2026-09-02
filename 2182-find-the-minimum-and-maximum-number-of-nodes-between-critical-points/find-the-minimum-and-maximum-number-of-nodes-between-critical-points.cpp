69da62c8613ae2ea6c67d3c95c6ac53c1162d9fc
DivyanshSharma13 <dsharma6_be23@thapar.edu>
2026-09-02 17:59:45 +0530

    Time: 587 ms (5.79%) | Memory: 128.3 MB (40.68%) - LeetSync

--- 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -1,36 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     vector<int> nodesBetweenCriticalPoints(ListNode* head) {
-        vector<int> help;
-
         ListNode* prev = head;
         ListNode* temp = head->next;
-        int count = 1;
-
-        while (temp && temp->next) {
+        int count=0;
+        vector<int>ans;
+        while(temp && temp->next){
             ListNode* fut = temp->next;
-
-            if ((temp->val > prev->val && temp->val > fut->val) ||
-                (temp->val < prev->val && temp->val < fut->val)) {
-                help.push_back(count);
+            if((prev->val<temp->val && temp->val>fut->val) || (prev->val>temp->val && temp->val<fut->val)){
+                ans.push_back(count);
             }
-
-            prev = temp;
-            temp = fut;
             count++;
+            prev = temp;
+            temp=fut;
         }
-
-        if (help.size() < 2)
-            return {-1, -1};
-
+        if(ans.size()<2){
+            return {-1,-1};
+        }
+        for(const auto& a: ans){
+            cout<<a<<" ";
+        }
         int mn = INT_MAX;
-
-        for (int i = 1; i < help.size(); i++) {
-            mn = min(mn, help[i] - help[i - 1]);
+        for(int i=1;i<ans.size();i++){
+            mn = min(mn,ans[i]-ans[i-1]);
         }
-
-        int mx = help.back() - help.front();
-
-        return {mn, mx};
+        int mx = ans.back()-ans.front();
+        return {mn,mx};
     }
 };
