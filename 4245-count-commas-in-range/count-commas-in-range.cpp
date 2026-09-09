90abd02fc7b0b66cb68e1e719a313f051f0e78fb
DivyanshSharma13 <dsharma6_be23@thapar.edu>
2026-09-09 12:43:52 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (84.70%) - LeetSync

--- 4245-count-commas-in-range/count-commas-in-range.cpp
+++ 4245-count-commas-in-range/count-commas-in-range.cpp
@@ -1,17 +1,12 @@
 class Solution {
 public:
     int countCommas(int n) {
-        int ans = 0;
-
-        for (int i = 1; i <= n; i++) {
-            int x = i;
-
-            while (x >= 1000) {
-                ans++;
-                x = x / 1000;
-            }
+        int count = 0;
+        while (n >=  1000) {
+            n--;
+            count++;
         }
 
-        return ans;
+        return count;
     }
 };
